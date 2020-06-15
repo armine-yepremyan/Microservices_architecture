ef589c2aecf1da151f6ac8c0d9cacead30a7c639
armyne <arminyepremyan@gmail.com>
2020-06-15 11:44:25 +0400

    Comment server creation

--- posts/index.js
+++ posts/index.js
@@ -13,7 +13,6 @@ app.get('/posts', (req, res) => {
 app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
-
     posts[id] = {
         id, title
     };
