40482d20dda3f11866bcb4856d7fa10ecc3faca7
armyne <arminyepremyan@gmail.com>
2020-06-15 21:48:04 +0400

    Post aand Comment creation on clien side with React

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -17,7 +19,7 @@ app.post('/posts/:id/comments', (req, res) => {
     
     const comments = commentsByPostId[req.params.id] || [];
     
-    comments.push({ ide: commentId, content})
+    comments.push({ id: commentId, content})
     commentsByPostId[req.params.id] = comments;
     res.status(201).send(comments);
 });
